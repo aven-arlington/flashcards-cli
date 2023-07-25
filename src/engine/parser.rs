d9c115a56d90dbe0ce4361605b4c30d7e68b8b36
Aven Arlington <aven.arlington@gmail.com>
2023-07-24 17:10:19 -0700

    Added json parsing capabilities (#2)

--- src/engine/parser.rs
+++ src/engine/parser.rs
@@ -1,17 +1,21 @@
 // JSON file parsing code for flashcards
-use std::path::PathBuf;
+use std::path::Path;
 use serde::Deserialize;
 use serde_json;
+use std::fs;
 
 #[derive(Deserialize, Debug)]
 pub struct FlashCard {
     pub clue_side: String,
     pub answer_side: String,
-    pub hint: String
+    pub hint: String,
+    pub level: String
 }
 
-pub fn parse_json(file_path:PathBuf) -> Result<Vec<FlashCard>, &'static str> {
-        let mut flashcards: Vec<FlashCard> = Vec::new();
-        Ok(flashcards)
-    }
+pub fn parse_json<P: AsRef<Path>>(file_path:P) -> Result<Vec<FlashCard>, &'static str> {
+    let file_data = fs::read_to_string(file_path).expect("Unable to read json file");
+    println!("File data: {}", file_data);
+    let flashcards: Vec<FlashCard> = serde_json::from_str(file_data.as_str()).expect("Unable to parse data string");
+    Ok(flashcards)
+}
 
