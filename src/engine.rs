a472e406fab8db90db7e3b33343548a92e395ee4
Aven Arlington <aven.arlington@gmail.com>
2023-07-26 07:43:44 -0700

    Sorting cards into levels (#3)

--- src/engine.rs
+++ src/engine.rs
@@ -1,17 +1,18 @@
 pub mod config;
 pub mod parser;
-
 use std::error::Error;
+use std::collections::BTreeMap;
 use crate::Config;
 use crate::engine::parser::FlashCard;
 
 pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
     println!("File_path: {}", config.file_path.as_path().display());
-    let flashcards: Vec<FlashCard> = parser::parse_json(config.file_path)?;
-
-    for card in flashcards {
-        println!("Clue : {}", card.clue_side);
+    let levels = parser::parse_json(config.file_path)?;
+    
+    for (level_number, cards) in levels {
+        for card in cards {
+            println!("Clue: {}", card.clue_side);
+        }
     }
     Ok(())
 }
-
--- src/engine/parser.rs
+++ src/engine/parser.rs
@@ -1,21 +1,52 @@
 // JSON file parsing code for flashcards
 use std::path::Path;
+use std::cmp::Ordering;
 use serde::Deserialize;
+use std::collections::BTreeMap;
 use serde_json;
 use std::fs;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Eq, Clone)]
 pub struct FlashCard {
     pub clue_side: String,
     pub answer_side: String,
     pub hint: String,
-    pub level: String
+    pub level: u32
 }
 
-pub fn parse_json<P: AsRef<Path>>(file_path:P) -> Result<Vec<FlashCard>, &'static str> {
+impl Ord for FlashCard {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.level.cmp(&other.level)
+    }
+}
+
+impl PartialOrd for FlashCard {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for FlashCard {
+    fn eq(&self, other: &Self) -> bool {
+        self.level == other.level
+    }
+}
+
+pub fn parse_json<P: AsRef<Path>>(file_path:P) -> Result<BTreeMap<u32, Vec<FlashCard>>, &'static str> {
     let file_data = fs::read_to_string(file_path).expect("Unable to read json file");
     println!("File data: {}", file_data);
-    let flashcards: Vec<FlashCard> = serde_json::from_str(file_data.as_str()).expect("Unable to parse data string");
-    Ok(flashcards)
+    let mut flashcards: Vec<FlashCard> = serde_json::from_str(file_data.as_str()).expect("Unable to parse data string");
+    if flashcards.is_empty() {
+        return Err("No FlashCards in json file");
+    }
+    flashcards.sort();
+
+    let mut level_map: BTreeMap<u32, Vec<FlashCard>> = BTreeMap::new();
+    for card in flashcards {
+        level_map.entry(card.level)
+            .and_modify(|level| level.push(card.clone()))
+            .or_insert(vec![card.clone()]);
+    }
+    Ok(level_map)
 }
 
