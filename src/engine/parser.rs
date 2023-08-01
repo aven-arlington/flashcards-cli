56284d768cb8303b6397a1f1f02d9258edb83907
Aven Arlington <aven.arlington@gmail.com>
2023-08-01 14:45:51 -0700

    Adding generic engine functionality (#4)

--- src/engine/parser.rs
+++ src/engine/parser.rs
@@ -34,7 +34,10 @@ impl PartialEq for FlashCard {
 
 pub fn parse_json<P: AsRef<Path>>(file_path:P) -> Result<BTreeMap<u32, Vec<FlashCard>>, &'static str> {
     let file_data = fs::read_to_string(file_path).expect("Unable to read json file");
+
+    // TODO Remove debug print
     println!("File data: {}", file_data);
+
     let mut flashcards: Vec<FlashCard> = serde_json::from_str(file_data.as_str()).expect("Unable to parse data string");
     if flashcards.is_empty() {
         return Err("No FlashCards in json file");
