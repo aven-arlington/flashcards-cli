16d4f4654342bec50cb0ba9b1700d8619e798aa8
Aven Arlington <aven.arlington@gmail.com>
2023-08-08 15:21:08 -0700

    Adding some randomization (#8)

--- src/engine.rs
+++ src/engine.rs
@@ -4,7 +4,7 @@ pub mod flashcard;
 use crate::engine::flashcard::FlashCard;
 use std::io::prelude::*;
 use std::error::Error;
-use log::{debug, error, log_enabled, info, Level};
+use log::{debug, log_enabled, Level};
 use rand::prelude::*;
 use crate::Config;
 
@@ -13,42 +13,47 @@ pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
     debug!("Parsing json file: {}", config.file_path.as_path().display());
     let cards = parser::parse_json(config.file_path)?;
 
-    print_cards(&cards);
+    if log_enabled!(Level::Debug) {
+        print_cards(&cards);
+    }
 
     println!("Welcome to FlashCards!");
     println!("Type \"Quit\" to exit the application.");
-    let mut cards_iter = cards.iter();
     let mut quit = false;
     let mut score: usize = 0;
-    let mut guess_count: usize = 0;
+    let mut wrong_count: usize = 0;
+    let mut rng = rand::thread_rng();
     println!("Lets begin!");
-    while !quit || guess_count < 3 {
-        let card = cards_iter.next().unwrap();
-        print!("{}: ", card.clue_side);
+    let mut card_index = rng.gen_range(0..cards.len());
+    while !quit && wrong_count < 3 {
+        print!("{}: ", cards[card_index].clue_side);
+
         let mut input = String::new();
         std::io::stdout().flush().expect("Flush failed");
         std::io::stdin().read_line(&mut input).expect("Failed to read input");
         let trimmed_input = input.trim();
         debug!("Input \"{}\"", trimmed_input);
-        debug!("Guess_count: {}", guess_count);
-        if trimmed_input == card.answer_side {
+
+        if trimmed_input == cards[card_index].answer_side {
             score += 1;
+            card_index = rng.gen_range(0..cards.len());
             println!("Correct! Current score: {}", score);
-        } else  if trimmed_input == "quit" {
+        } else if trimmed_input == "quit" {
             println!("Quitting application");
             quit = true;
         } else {
-            guess_count += 1;
+            wrong_count += 1;
             println!("Incorrect, your input was \"{}\"", trimmed_input);
-            debug!("Guess_count: {}", guess_count);
+            debug!("Guess_count: {}", wrong_count);
         }
     }
 
+    println!("Game Over");
     Ok(())
 }
 
 pub fn print_cards(cards:&Vec<FlashCard>) {
     for card in cards {
-        debug!("{}", card.clue_side);
+        println!("{}", card.clue_side);
     }
 }
