01eb86049ea684bc5049d6f886b216ead758c509
Aven Arlington <aven.arlington@gmail.com>
2023-08-10 17:56:47 -0700

    Adding a deck class to handle shuffling etc

--- src/engine.rs
+++ src/engine.rs
@@ -1,32 +1,31 @@
 pub mod config;
 pub mod parser;
 pub mod flashcard;
-use crate::engine::flashcard::FlashCard;
+pub mod deck;
+use crate::engine::deck::Deck;
 use std::io::prelude::*;
 use std::error::Error;
 use log::{debug, log_enabled, Level};
-use rand::prelude::*;
 use crate::Config;
 
 
 pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
     debug!("Parsing json file: {}", config.file_path.as_path().display());
-    let cards = parser::parse_json(config.file_path)?;
+    let mut deck = Deck::new(config.file_path).expect("Could not create FlashCard Deck");
 
     if log_enabled!(Level::Debug) {
-        print_cards(&cards);
+        deck.print_cards();
     }
 
     println!("Welcome to FlashCards!");
     println!("Type \"quit\" to exit the application.");
+    println!("Lets begin!");
     let mut quit = false;
     let mut score: usize = 0;
     let mut wrong_count: usize = 0;
-    let mut rng = rand::thread_rng();
-    println!("Lets begin!");
-    let mut card_index = rng.gen_range(0..cards.len());
     while !quit && wrong_count < 3 {
-        print!("{}: ", cards[card_index].clue_side);
+        let card = deck.random_card().unwrap();
+        print!("{}: ", card.clue_side);
 
         let mut input = String::new();
         std::io::stdout().flush().expect("Flush failed");
@@ -34,9 +33,8 @@ pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
         let trimmed_input = input.trim();
         debug!("Input \"{}\"", trimmed_input);
 
-        if trimmed_input == cards[card_index].answer_side {
+        if trimmed_input == card.answer_side {
             score += 1;
-            card_index = rng.gen_range(0..cards.len());
             println!("Correct! Current score: {}", score);
         } else if trimmed_input == "quit" {
             println!("Quitting application");
@@ -46,14 +44,9 @@ pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
             println!("Incorrect, your input was \"{}\"", trimmed_input);
             debug!("Guess_count: {}", wrong_count);
         }
+        
     }
 
     println!("Game Over");
     Ok(())
-}
-
-pub fn print_cards(cards:&Vec<FlashCard>) {
-    for card in cards {
-        println!("{}", card.clue_side);
-    }
 }
