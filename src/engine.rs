d6c9692455a55a40a41db6e3766c438ba308c29a
Aven Arlington <aven.arlington@gmail.com>
2023-10-31 14:18:27 -0700

    Adding basic levels (#17)

--- src/engine.rs
+++ src/engine.rs
@@ -12,6 +12,7 @@ pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
     let mut deck = Deck::new(config.cards).expect("Could not create FlashCard Deck");
 
     if log_enabled!(Level::Debug) {
+        debug!("Config object data");
         debug!("attempts_before_hint: {}", config.attempts_before_hint);
         debug!("attempts_before_wrong: {}", config.attempts_before_wrong);
         deck.print_cards();
@@ -21,10 +22,10 @@ pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
     println!("Type \"quit\" to exit the application.");
     println!("Lets begin!");
     let mut quit = false;
-    let mut score: usize = 0;
+    let mut consecutive_right: usize = 0;
     let mut wrong_count: usize = 0;
     while !quit && wrong_count < 3 {
-        let card = deck.random_card().unwrap();
+        let card = deck.random_card_from_current_level_range().unwrap();
         print!("{}: ", card.clue_side);
 
         let mut input = String::new();
@@ -34,8 +35,8 @@ pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
         debug!("Input \"{}\"", trimmed_input);
 
         if trimmed_input == card.answer_side {
-            score += 1;
-            println!("Correct! Current score: {}", score);
+            consecutive_right += 1;
+            println!("Correct! You have gotten {} right in a row!", consecutive_right);
         } else if trimmed_input == "quit" {
             println!("Quitting application");
             quit = true;
@@ -43,6 +44,11 @@ pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
             wrong_count += 1;
             println!("Incorrect, your input was \"{}\"", trimmed_input);
             debug!("Guess_count: {}", wrong_count);
+        }
+
+        if consecutive_right >= 5 {
+            consecutive_right = 0;
+            deck.increase_level_max();
         }
         
     }
--- src/engine/deck.rs
+++ src/engine/deck.rs
@@ -1,10 +1,13 @@
 use crate::engine::flashcard::FlashCard;
+use std::ops::Range;
 use log::debug;
 use rand::prelude::*;
 
 #[derive(Debug)]
 pub struct Deck {
     cards: Vec<FlashCard>,
+    level_indexes: Vec<usize>,
+    level_range: Range<usize>,
     rng: ThreadRng
 }
 
@@ -15,8 +18,25 @@ impl Deck {
         }
         cards.sort();
         
+        let mut level_indexes = Vec::new();
+        for (i, card) in cards.iter().enumerate() {
+            if level_indexes.is_empty() {
+                level_indexes.push(i);
+                continue;
+            }
+            if card.level > cards[level_indexes[level_indexes.len()-1]].level {
+                level_indexes.push(i);
+                continue;
+            }
+        }
+
+        let level_range = Range { start: 0, end: 1 };
+
+
         Ok(Self { 
             cards,
+            level_indexes,
+            level_range,
             rng:rand::thread_rng(),
         })
     }
@@ -26,13 +46,48 @@ impl Deck {
     }
 
     pub fn print_cards(&self) {
-        for card in &self.cards {
-            debug!("{}", card.clue_side);
+        for (i, card) in self.cards.iter().enumerate() {
+            debug!("Deck position {} - {}", i, card.clue_side);
+        }
+        debug!("Level Stratifications:");
+        for index in &self.level_indexes {
+            debug!("Deck index {}", index);
         }
     }
 
     pub fn random_card(&mut self) -> Result<FlashCard, &'static str> {
         let card_index = self.rng.gen_range(0..self.cards.len());
+        Ok(self.cards.get(card_index).expect("Flashcard does not exist").clone())
+    }
+
+    pub fn increase_level_max(&mut self) {
+        if self.level_range.end >= self.level_indexes.len() -1 {
+            println!("The maximum level has been reached");
+            return;
+        }
+        println!("The maximum difficutly has increased!");
+        self.level_range.end += 1;
+    }
+
+    pub fn increase_level_min(&mut self) {
+        if self.level_range.start > self.level_range.end {
+            println!("The minimum level cannot be higher than the maximum.");
+            return;
+        }
+        if self.level_range.start >= self.level_indexes.len() -1 {
+            println!("The highest minimum level has been reached");
+            return;
+        }
+        println!("The minimum difficutly has increased!");
+        self.level_range.start += 1;
+    }
+
+    pub fn reset_level(&mut self) {
+        self.level_range = Range{ start: 0, end: 0 };
+    }
+
+    pub fn random_card_from_current_level_range(&mut self) -> Result<FlashCard, &'static str> {
+        let card_index = self.rng.gen_range(self.level_indexes[self.level_range.start]..self.level_indexes[self.level_range.end]);
         Ok(self.cards.get(card_index).expect("Flashcard does not exist").clone())
     }
 }
