b760907f1a56c141a525768c16fab24956b05f84
Aven Arlington <aven.arlington@gmail.com>
2023-10-31 14:16:51 -0700

    Adding basic levels

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,10 @@ fn main() {
         .format(|buf, record| {
             writeln!(
                 buf,
-                "{}:{} {} [{}] - {}",
+                "[{}] {}:{} - {}",
+                record.level(),
                 record.file().unwrap_or("unknown"),
                 record.line().unwrap_or(0),
-                chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                record.level(),
                 record.args()
             )
         })
